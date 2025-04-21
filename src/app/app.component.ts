ed623cbd508b27b6a4c7eb1810ecd8d6ec82e06c
Felippe <felippepg.goncalves@outlook.com.br>
2025-04-21 15:15:09 -0300

    feat: add AppState implementation and fix dataState prop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'serverManagementApp';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
+  constructor(private serverService: ServerService) {}
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$.pipe(
+      map((response) => {
+        return { dataState: DataState.LOADED, appData: response };
+      }),
+      startWith({ dataState: DataState.LOADING }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR, error });
+      })
+    );
+  }
 }
